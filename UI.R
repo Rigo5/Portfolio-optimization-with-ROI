5cc2d2c8c2ca8050ad998a496c98b17db7e31876
Rigo5 <micheleprisco1@gmail.com>
2020-08-04 09:00:06 +0200

    adding different input

--- UI.R
+++ UI.R
@@ -9,34 +9,60 @@ ui = fluidPage(
   title = 'Markowitz Portfolio with ROI S&P 500',
   fluidRow(
     column(
-      6,
+      4,
       selectInput(
         'Port_stocks',
         'Stocks for Portfolio',
         multiple = TRUE,
-        choices = ticker,
-        selected = ticker[1:10]
+        choices = names(ticker_list),
+        selected = names(ticker_list)[1:10]
       )
     ),
     column(
-      6,
+      4,
       dateInput(
         'Start_date',
         'From',
         value = '2017-01-04'
       )
+    ),
+    column(
+      4,
+      actionButton("go", "Go")
     )
   ),
   fluidRow(
     plotlyOutput('Cumulated_returns')
   ),
   fluidRow(
-    numericInput(
-      'Sample',
-      'Sample dimension',
-      value = 300,
-      min = 300,
-      max = 600
+    column(
+      4,
+      numericInput(
+        'Sample',
+        'Sample dimension',
+        value = 300,
+        min = 300,
+        max = 600
+      )
+    ),
+    column(
+      4,
+      selectInput(
+        'Type_port',
+        'Strategy',
+        values = c('Long Only', 'Short'),
+        selected = 'Long Only'
+      )
+    ),
+    column(
+      4,
+      numericInput(
+        'Beta',
+        'Risk aversion',
+        value = 0.5,
+        min = 0.000001,
+        max = 1
+      )
     )
   ),
   fluidRow(
